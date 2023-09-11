e2e74fabc043c154ca34ccdd3f85b1bd339837dc
feng <15757337126@163.com>
2023-09-11 17:34:23 +0800

    调试node环境污染问题

--- cde/test.js
+++ cde/test.js
@@ -4,29 +4,39 @@
 // 3、在脚本中不带var设置window，则认为是给 global加了window属性，window变成了全局变量
 // 如何让局部的window拥有global的方法----解决部分环境污染问题？
 // 如何让eval中的代码设置的全局变量不设置到全局，但是可以通过变量名直接获取？
+    // 设置window.global?  无用
+    // 在vm代码执行前，将global赋值给指定变量？对 global重新赋值无用
+// 使用vm隔离环境，在代码中设置全局变量，是否被污染？---会
+// 使用sandbox可以实现，内部设置全局变量不会污染global；
+    // 沙箱设置的全局变量能否在eval使用？可以
+    // 在沙箱环境中执行代码，不污染global；且设置的全局变量可以直接通过变量名获取
+    
 
-
+// 057310086
 console.log('----------start-------------');
-var window = {a: 'aaaa'}
-Object.defineProperties(window, {
-    [Symbol.toStringTag]: {
-        value: 'Window',
-        configurable: true
-    },
-    get(target, prop, receiver) {
-            return target[prop] || global[prop];
-        },
-});
-console.log(window['a']);
-console.log(window.a);
-// const test1 = require('./test1');
-// const test2 = require('./test2');
+// var window = {a: 'aaaa',global: {bbb: 'bbbbbb'}}
+// Object.defineProperties(window, {
+//     [Symbol.toStringTag]: {
+//         value: 'Window',
+//         configurable: true
+//     },
+//     get(target, prop, receiver) {
+//             return target[prop] || global[prop];
+//         },
+// });
+// console.log(window['a']);
+// console.log(window.a);
 
-// console.log('ready for exeuting test1');
-// test1.test1_a_1();
 
-// console.log('ready for exeuting test2');
-// test2.test2_a_1();
+
+const test1 = require('./test1');
+const test2 = require('./test2');
+
+console.log('ready for exeuting test1');
+test1.test1_a_1();
+
+console.log('ready for exeuting test2');
+test2.test2_a_1();
 
 console.log('-----------ending-----------');
 
--- cde/test1.js
+++ cde/test1.js
@@ -1,17 +1,29 @@
+const vm = require('vm');
 
-var a1 = 'test111111';
-var window = {
-    a1: a1
-};
+
+// var a1 = 'test111111';
+var window = {a: 'aaaa'};
+window.window = window;
+
+const sandbox = window;
+
+vm.createContext(sandbox);
+
+
 
 function a_1(){
-    // debugger;
+    debugger;
     console.log('Now enter in test1');
-    console.log('In test1 window[a1]: ', window['a1']);
-    console.log('In test1 window.a1: ', window.a1);
-    console.log('In test1 a1: ', a1);
-    vm_code = 'console.log("In test1 eval: a1 -> ", a1);'
-    eval( vm_code)
+    console.log('In test1 window[a]: ', window['a']);
+    console.log('In test1 window.a: ', window.a);
+    // console.log('In test1 a1: ', a1);
+    // vm_code = 'debugger;console.log("In test1 eval: bbb -> ", bbb);';
+    // vm_code = "vm_a2 = 'test1_vm_code';debugger;"
+    // eval(vm_code);
+    // var vm_code =  'debugger;console.log("设置全局变量bbb");' + 'bbb="bbbbbb";'  + "" + 'eval("debugger;console.log(' + "'bbb -> ', bbb, 'window.a -> ', window.a);" + '");';
+    var vm_code = 'debugger;console.log("window.Math -> ", window["Math"]);';
+    vm.runInContext(vm_code, sandbox);
+    // vm.runInContext('debugger;console.log("bbb -> ", bbb);', sandbox);
     console.log('Now exit from test1');
 }
 
