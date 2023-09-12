8bf9f9a7039dd3a9f9dd8fd4156c8f58e02a75f9
feng <15757337126@163.com>
2023-09-12 16:02:58 +0800

    cde网站接口调试； node全局污染解决

--- cde/test.py
+++ cde/test.py
@@ -60,29 +60,42 @@ def home() -> None:
         f.write(resp1.content)
 
     print('开始请求详情页')
-    # with open("/home/feng/workspace/myWeb/cde/success_home.html", "wb") as f:
-    #     f.write(resp.content)
+
+
+    detail_common_args = '&sort=desc&sort2=&rule=CTR&secondLevel=0&currentpage=1&keywords=&reg_no=&indication=&case_no=&drugs_name=&drugs_type=&appliers=&communities=&researchers=&agencies=&state='
+    detail_args = {
+        'id': 'c2ea6959de674662a681e758256db3af',
+        'ckm_index': '1',
+    }
+    payload = urllib.parse.urlencode(
+        detail_args) + detail_common_args
+
+    detail_url = "http://www.chinadrugtrials.org.cn/clinicaltrials.searchlistdetail.dhtml"
+
     cookie_t_str = getCookie(resp1.text,'/v2/cde_detail_cookie')
     if cookie_t_str:
-        # with open("/home/feng/workspace/myWeb/gansu_samr/detail_cookie", "r") as f:
-        #     cookie_t_str = f.read()
+        detail_headers = {
+    "Host": "www.chinadrugtrials.org.cn",
+    "Proxy-Connection": "keep-alive",
+    "Cache-Control": "max-age=0",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+    "Origin": "http://www.chinadrugtrials.org.cn",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Referer": "http://www.chinadrugtrials.org.cn/clinicaltrials.searchlist.dhtml",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9"
+}
+
         cookies_t = {cookie_t_str.split('=')[0]: cookie_t_str.split("=")[1].split(';')[0]}
-        cookies = {**cookies_s, **cookies_t}
+        detail_cookies = {**cookies_s, **cookies_t}
 
-        detail_common_args = '&sort=desc&sort2=&rule=CTR&secondLevel=0&currentpage=1&keywords=&reg_no=&indication=&case_no=&drugs_name=&drugs_type=&appliers=&communities=&researchers=&agencies=&state='
-        detail_args = {
-            'id': '1967ac1f28cc42da9fd5c8e4366f92ac',
-            'ckm_index': '1',
-        }
-        payload = urllib.parse.urlencode(
-            detail_args) + detail_common_args
-
-        url = "http://www.chinadrugtrials.org.cn/clinicaltrials.searchlistdetail.dhtml"
-
-        resp = requests.post(url, headers=headers,params=payload, timeout=20, cookies=cookies)
-        print(resp.status_code)
+        print(cookies)
+        resp2 = requests.post(detail_url, headers=detail_headers, params=payload, timeout=20, cookies=detail_cookies)
+        print(resp2.status_code)
         with open("/home/feng/workspace/myWeb/cde/success_detail.html", "wb") as f:
-            f.write(resp1.content)
+            f.write(resp2.content)
     else:
         print("cookie 异常")
 
--- cde/test1.js
+++ cde/test1.js
@@ -1,18 +1,19 @@
 const vm = require('vm');
 
 
-// var a1 = 'test111111';
-var window = {a: 'aaaa'};
-window.window = window;
-
-const sandbox = window;
-
-vm.createContext(sandbox);
 
 
 
 function a_1(){
-    debugger;
+    // var a1 = 'test111111';
+    var window = Object.assign({a: 'aaaa'}, global);
+    window.window = window;
+    window['test1111'] = 'test1111';
+
+    const sandbox = window;
+
+    vm.createContext(sandbox);
+
     console.log('Now enter in test1');
     console.log('In test1 window[a]: ', window['a']);
     console.log('In test1 window.a: ', window.a);
@@ -21,8 +22,8 @@ function a_1(){
     // vm_code = "vm_a2 = 'test1_vm_code';debugger;"
     // eval(vm_code);
     // var vm_code =  'debugger;console.log("设置全局变量bbb");' + 'bbb="bbbbbb";'  + "" + 'eval("debugger;console.log(' + "'bbb -> ', bbb, 'window.a -> ', window.a);" + '");';
-    var vm_code = 'debugger;console.log("window.Math -> ", window["Math"]);';
-    vm.runInContext(vm_code, sandbox);
+    // var vm_code = 'debugger;function aaa(){xxx=111};function bbb(){return xxx+1}; aaa();console.log(bbb());';
+    // vm.runInContext(vm_code, sandbox);
     // vm.runInContext('debugger;console.log("bbb -> ", bbb);', sandbox);
     console.log('Now exit from test1');
 }
